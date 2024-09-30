4f71466d7a48c359dcda1fc3b349ad967695ed67
Tadeu Jerônimo <tadeus.jeronimo@gmail.com>
2024-09-30 12:53:19 -0300

    Additional comments

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,5 +1,16 @@
 import "./Button.css";
 
+/**
+ * Um componente de botão reutilizável que aceita várias props para personalização.
+ *
+ * @param {object} props - Um objeto contendo props para o componente de botão.
+ * @param {string} props.type - O tipo de botão (ex.: "button", "submit", etc.).
+ * @param {string} props.className - O nome da classe CSS para o botão.
+ * @param {function} props.onClick - O manipulador de eventos para o evento de clique do botão.
+ * @param {string} props.icon - O nome da classe CSS para o ícone.
+ * @param {string} props.text - O conteúdo de texto do botão.
+ * @return {JSX.Element} O componente de botão renderizado.
+ */
 const Button = ({ ...props }) => {
   return (
     <button type={props.type || "button"} className={props.className} onClick={props.onClick}>
--- src/components/FormInput/FormInput.js
+++ src/components/FormInput/FormInput.js
@@ -1,5 +1,16 @@
 import "./FormInput.css";
 
+/**
+ * Um componente de entrada de formulário reutilizável que renderiza um rótulo e um campo de entrada.
+ *
+ * @param {object} props - As props do componente.
+ * @param {string} props.inputName - O nome e id do campo de entrada.
+ * @param {string} props.label - O texto a ser exibido como rótulo.
+ * @param {string} props.type - O tipo do campo de entrada.
+ * @param {string} props.placeholder - O texto de placeholder para o campo de entrada.
+ * @param {boolean} props.required - Se o campo de entrada é obrigatório.
+ * @return {JSX.Element} O elemento JSX que representa o componente de entrada de formulário.
+ */
 const FormInput = ({ ...props }) => {
   return (
     <div className="form-input">
